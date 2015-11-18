4577f5f24afc1261c01cc951ce5f1b29a9cc0b7a
Benjamin Reynolds <breyno127@gmail.com>
2015-11-18 20:36:45 +0000

    Update recipe tests

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 	
 	def setup
-		@recipe = Recipe.new(name: "chicken parm", summary: "This is the best chicken parm recipe ever", 
+		@chef = Chef.create(chefname: "bob", email: "bob@example.com")
+		@recipe = @chef.recipes.build(name: "chicken parm", summary: "This is the best chicken parm recipe ever", 
 							description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
 	end
 	
@@ -53,6 +54,11 @@ class RecipeTest < ActiveSupport::TestCase
 	
 	test "description length should not be too short" do
 		@recipe.description = "a" * 19
+		assert_not @recipe.valid?
+	end
+	
+	test "chef_id should be present" do
+		@recipe.chef_id = nil
 		assert_not @recipe.valid?
 	end
 	
