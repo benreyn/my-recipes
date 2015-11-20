6b8d85dd668599c6524621b4ddf5751292fb0fb1
Benjamin Reynolds <breyno127@gmail.com>
2015-11-20 01:20:32 +0000

    implement likes feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -39,6 +39,13 @@ class RecipesController < ApplicationController
 		end
 	end
 	
+	def like
+		@recipe = Recipe.find(params[:id])
+		Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+		# flash[:success] = "Your selection was succesful"
+		redirect_to :back
+	end
+	
 	private
 	
 		def recipe_params
