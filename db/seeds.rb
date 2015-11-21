f3253d669859a575854b30be126b1bbf5c4f2d35
Benjamin Reynolds <breyno127@gmail.com>
2015-11-21 12:45:23 -0500

    Add seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+chefs = Chef.create([{chefname: "bob", email: "bob@example.com"},
+                     {chefname: "john", email: "john@example.com"},
+                     {chefname: "bill", email: "bill@example.com"}])
+
+chef = Chef.first
+(1..3).each do |n|
+  chef.recipes.create(name: "example-recipe-#{n}",
+                      summary: "summary for example-recipe-#{n}",
+                      description: "Steps for example-recipe-#{n}
+                                    step 1. Do some stuff
+                                    step 2. Do some more stuff
+                                    step 3. Finish it up")
+end
+chef = Chef.find(2)
+(4..6).each do |n|
+  chef.recipes.create(name: "example-recipe-#{n}",
+                      summary: "summary for example-recipe-#{n}",
+                      description: "Steps for example-recipe-#{n}
+                                    step 1. Do some stuff
+                                    step 2. Do some more stuff
+                                    step 3. Finish it up")
+end
+chef = Chef.last
+(7..10).each do |n|
+  chef.recipes.create(name: "example-recipe-#{n}",
+                      summary: "summary for example-recipe-#{n}",
+                      description: "Steps for example-recipe-#{n}
+                                    step 1. Do some stuff
+                                    step 2. Do some more stuff
+                                    step 3. Finish it up")
+end
