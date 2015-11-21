8d542e04c71c0cc49fab73bd0d1a84dc28c06f3b
Benjamin Reynolds <breyno127@gmail.com>
2015-11-21 15:12:12 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  
+
   get '/home', to: 'pages#home'
-  
+
   resources :recipes do
     member do
       post 'like'
     end
   end
+
+  resources :chefs, except: [:new]
   
-  
-  
+  get '/register', to: 'chefs#new'
+
 end
