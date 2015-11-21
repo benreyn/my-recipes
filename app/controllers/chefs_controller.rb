968c75530c201f8df09cc193e3d30759be47ae80
Benjamin Reynolds <breyno127@gmail.com>
2015-11-21 16:10:51 -0500

    Create chefs controller and associated new and edit views

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,37 @@
+class ChefsController < ApplicationController
+
+  def new
+    @chef = Chef.new
+  end
+
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Your account has been created succesfully"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your profile has been updated succesfully"
+      redirect_to recipes_path # TODO change to show chef page
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def chef_params
+      params.require(:chef).permit(:chefname, :email, :password)
+    end
+
+end
